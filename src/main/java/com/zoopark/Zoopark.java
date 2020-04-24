8ce313b135ea69d06df1d29933e5c6074c0e50cf
Creater <mwww-go@yandex.ru>
2020-04-25 02:42:06 +0400

    add interface

---
+++ src/main/java/com/zoopark/Zoopark.java
@@ -0,0 +1,16 @@
+package com.zoopark;
+
+
+interface Zoopark {
+
+        void addAnimal(Animal a);// добавить животное в вольер
+        void showAll(); //показать содержимое
+        void removeAnimal(Animal a);//удалить животное
+
+    //
+    //Говорить
+    //накормить животных
+    //создать животное
+    //создание вольеров
+}
+
